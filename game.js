3b87281954610bfd9ad8ae1663accd3375f6d4af
Gabriela P <gabriela.poryazova@gmail.com>
2022-07-29 17:49:58 +0300

    Fix formula error

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
 //get random computer choice
 function getComputerChoice (){
 const options = ['rock', 'paper', 'scissors'];
-let index = Math.floor(Math.random() * 4);
-console.log(options[index]);
-console.log(index)
+let index = Math.floor(Math.random() * 3);
+return options[index]
 }
 getComputerChoice()
